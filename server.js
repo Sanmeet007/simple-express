34bd76571e1a6f6886cb01aeffc7a59d4cde7ede
Sanmeet007 <ssanmeet123@gmail.com>
2022-09-25 01:15:09 +0530

    Added ejs support

--- server.js
+++ server.js
@@ -2,9 +2,28 @@ import express from "./lib/express.js";
 
 const app = express();
 app.setStatic("public");
+app.setViewsDir("views");
+
 app.get("/", (req, res) => {
-  return res.send("Hello world");
+  return res.renderFile("index", {
+    val: 10,
+  });
 });
-app.post("/", (req, res) => {});
+
+app.post("/", (req, res) => {
+  return res.json({
+    id: "1",
+    user: "admin",
+  });
+});
+
+app.use((req, res) => {
+  if (req.method != "POST" || req.method != "GET") return res.error(501);
+  else return res.error(404);
+});
+
+app.error(404, (_, res) => {
+  return res.sendFile("public/404.html");
+});
 
 app.listen(2000);
