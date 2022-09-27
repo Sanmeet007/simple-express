b5d91c31d0c6bda755f6387861bda93fdb47bfaf
Sanmeet007 <ssanmeet123@gmail.com>
2022-09-28 00:41:14 +0530

    updated packages and example

--- example/server.js
+++ example/server.js
@@ -1,23 +1,37 @@
 import express from "@sanmeet007/simple-express";
 
 const app = express();
-app.setStatic("public");
-app.setViewsDir("views");
 
-app.get("/", (req, res) => {
-  if (req.query) console.log("GET data : ", req.query);
-  return res.renderFile("index", {
-    tool: "EJS",
-    string: "",
-  });
+app.setStatic("public"); // setting public dir
+app.setViewsDir("views"); // setting views dir
+
+// Now server supports only get and posts requests
+app.use((req, res, next) => {
+  if (req.method != "GET" && req.method != "POST") return res.error(501);
 });
 
-app.get("/:string", (req, res, next) => {
-  const string = req.params.string;
-  return res.renderFile("index", {
-    tool: "EJS",
-    string,
-  });
+app.get("/", (req, res) => {
+  if (req.query) console.log("GET data : ", req.query); // wildcard urls
+  return res.sendFile("public/index.html");
+});
+
+app.route("/user", "GET", (req, res) => {
+  return res.json({
+    id: 0,
+    isAdmin: true,
+    data: "...",
+  }); // sending a json response
+});
+
+app.get("/:id", (req, res, next) => {
+  const id = req.params.id;
+  if (!isNaN(id)) {
+    return res.renderFile("index", {
+      tool: "EJS",
+      string: id,
+    });
+  }
+  return res.error(404);
 });
 
 app.post("/", (req, res) => {
@@ -27,34 +41,25 @@ app.post("/", (req, res) => {
     });
   }
 
-  if (req.hasOwnProperty("body")) {
+  if (req.body != null) {
     console.log(req.body);
   }
 
   return res.redirect("/");
 });
 
-app.use((req, res, next) => {
-  console.log("Logging request url : ", req.url);
-});
-
-app.route("/", "PUT", (req, res) => {
-  console.log("PUT request encountered");
-  return res.send("Method not implemented", {}, 501);
-});
-
 app.error(404, (_, res) => {
-  return res.sendFile("public/index.html", 404);
+  return res.sendFile("public/errors/404.html", 404);
 });
 
 app.error(500, (_, res) => {
-  return res.json(
-    {
-      error: true,
-      message: "Internal server error",
-    },
-    500
-  );
+  return res.sendFile("public/errors/500.html", 500);
 });
 
-app.listen(2000);
+app.error(501, (_, res) => {
+  return res.sendFile("public/errors/501.html", 500);
+});
+
+app.listen(2000, "localhost", (e) => {
+  console.log("Simple express rocks !");
+}); // Listening for requests
