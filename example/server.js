8fe5edabb7c9b327bc7d383686fdf4a7418648e1
Sanmeet007 <ssanmeet123@gmail.com>
2022-09-27 19:45:11 +0530

    bug fixes

--- example/server.js
+++ example/server.js
@@ -21,13 +21,12 @@ app.get("/:string", (req, res, next) => {
 });
 
 app.post("/", (req, res) => {
-  if (req.files && req.files.hasOwnProperty("file")) {
+  if (req.files != null && req.files.hasOwnProperty("file")) {
     req.files["file"].forEach((file) => {
       file.upload();
     });
-  } else {
-    console.log("No files were attached");
   }
+
   if (req.hasOwnProperty("body")) {
     console.log(req.body);
   }
